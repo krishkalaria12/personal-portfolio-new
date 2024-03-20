e11a2227988fe7531132c84aa4aa86bee1cf09fb
CodeWithKrish12 <codewithkrish12@gmail.com>
2024-03-20 21:03:21 +0530

    fixed minor bugs during build

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 import React from "react";
 import { TracingBeam } from "@/components/ui/tracing-beam";
-import { SpotlightPreview } from "@/Pages/SpolightSection";
+import SpotlightPreview from "@/Pages/SpolightSection";
 import ProjectSection from "@/Pages/ProjectSection";
 import ContactSection from "@/Pages/ContactSection";
-import { SkillsSection } from "@/Pages/SkillsSection";
+import SkillsSection from "@/Pages/SkillsSection";
 import AboutSection from "@/Pages/AboutSection"
 
 export default function Home() {
